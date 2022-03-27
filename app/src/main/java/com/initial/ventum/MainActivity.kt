e4a6a41632b767ff271d28d1ca5aadb8d82b62b0
Josh Dorn <jr_dorn@yahoo.com>
2022-03-27 17:22:46 -0500

    Create new Compose activity

---
+++ app/src/main/java/com/initial/ventum/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.initial.ventum
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.initial.ventum.ui.theme.VentumTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            VentumTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    VentumTheme {
+        Greeting("Android")
+    }
+}
--- app/src/test/java/com/initial/ventum/ExampleUnitTest.kt
+++ app/src/test/java/com/initial/ventum/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.myapp
+package com.initial.ventum
 
 import org.junit.Test
 
